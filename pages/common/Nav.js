93178b50ad46ff140febc948881e42d81e05e5d4
kwon-hyemin <kwon9184h@gmail.com>
2022-04-18 18:42:53 +0900

    2022-04-18

--- pages/common/Nav.js
+++ pages/common/Nav.js
@@ -9,12 +9,26 @@ export default function Nav(){
     <li className={styles.li}> <Link href='/basic/counter'>카운터</Link> </li>
     <li className={styles.li}> <Link href='/basic/calc'>계산기</Link> </li>
     <li className={styles.li}> <Link href='/basic/bmi'>BMI</Link> </li>
-    <li className={styles.li}> <Link href='/todo/todo'>투두리스트</Link> </li>
-    <li className={styles.li}> <Link href='/board/board'>게시글등록</Link> </li>
+    <li className={styles.li}> <Link href='/game/gameArticles'>게임추가</Link> </li>
+    <li className={styles.li}> <Link href='/game/getGames'>게임목록</Link> </li>
+    <li className={styles.li}> <Link href='/game/modifyGame'>게임수정</Link> </li>
+    <li className={styles.li}> <Link href='/game/removeGame'>게임추가</Link> </li>
+    <li className={styles.li}> <Link href='/game/team-form'>팀추가</Link> </li>
+    <li className={styles.li}> <Link href='/game/team-list'>팀리스트</Link> </li>
+    <li className={styles.li}> <Link href='/todo/addTodo'>투두추가</Link> </li>
+    <li className={styles.li}> <Link href='/todo/getTodos'>투두목록</Link> </li>
+    <li className={styles.li}> <Link href='/todo/modifyTodo'>투두수정</Link> </li>
+    <li className={styles.li}> <Link href='/todo/removeTodo'>투두삭제</Link> </li>
+    <li className={styles.li}> <Link href='/board/boardArticles'>게시글추가</Link> </li>
+    <li className={styles.li}> <Link href='/board/getBoard'>게시글목록</Link> </li>
+    <li className={styles.li}> <Link href='/board/modifyBoard'>게시글수정</Link> </li>
+    <li className={styles.li}> <Link href='/board/removeBoard'>게시글삭제</Link> </li>
     <li className={styles.li}> <Link href='/board/board-list'>게시글목록</Link> </li>
     <li className={styles.li}> <Link href='/user/join'>회원가입</Link> </li>
     <li className={styles.li}> <Link href='/user/login'>로그인</Link> </li>
-    <li className={styles.li}> <Link href='/user/user-list'>사용자목록</Link> </li>
+    <li className={styles.li}> <Link href='/user/getUsers'>사용자목록</Link> </li>
+    <li className={styles.li}> <Link href='/user/updateUser'>사용자수정</Link> </li>
+    <li className={styles.li}> <Link href='/user/withdrawUser'>혜민탈퇴</Link> </li>
   </ul>
     </nav>
   );
